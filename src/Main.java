f658ab92bf04cdd649f8d172bc3acf910733e4a0
mateusz <wozniak.mateusz1996@gmail.com>
2019-11-16 11:28:14 +0100

    test

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,9 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        
+        test();
     }
-
+    public static void test() {
+        System.out.println("test");
+    }
 }
